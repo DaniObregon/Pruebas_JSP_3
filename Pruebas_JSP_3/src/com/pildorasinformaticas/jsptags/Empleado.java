5991e04410c97ab1f472636ca84d5c7b060e40cc
Dani <73905910+DaniObregon@users.noreply.github.com>
2021-03-10 18:53:27 -0300

    Update Empleado.java

--- Pruebas_JSP_3/src/com/pildorasinformaticas/jsptags/Empleado.java
+++ Pruebas_JSP_3/src/com/pildorasinformaticas/jsptags/Empleado.java
@@ -17,39 +17,49 @@ public class Empleado {
 		this.bonus = bonus;
 		this.total = total;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getApellido() {
 		return apellido;
 	}
+	
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+	
 	public String getPuesto() {
 		return puesto;
 	}
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
 	}
+	
 	public double getSalario() {
 		return salario;
 	}
+	
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
+	
 	public double getBonus() {
 		return bonus;
 	}
+	
 	public void setBonus(double bonus) {
 		this.bonus = bonus;
 	}
+	
 	public double getTotal() {
 		return total;
 	}
+	
 	public void setTotal(double total) {
 		this.total = total;
 	}
